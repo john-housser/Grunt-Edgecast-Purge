93f4f45a7afeee5712a14f117dd5690af05ac7be
John Housser <John.Housser@arcteryx.com>
2015-02-25 10:50:06 -0800

    Actual functionality

--- tasks/edgecast_purge.js
+++ tasks/edgecast_purge.js
@@ -8,43 +8,87 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function ( grunt ) {
+  var http = require( 'http' ),
+    async = require('async');
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('edgecast_purge', 'Purge files from the Edgecast CDN', function() {
+  grunt.registerMultiTask( 'edgecast_purge', 'Purge files from the Edgecast CDN', function () {
+    var done = this.async();
+
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var options = this.options( {
+      host: 'api.edgecast.com',
+      port: 80,
+      method: 'PUT'
+    } );
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    var reqOptions = {
+      host: options.host,
+      port: options.port,
+      path: '/v2/mcc/customers/' + options.customer + '/edge/purge',
+      method: options.method,
+      headers: {
+        'Authorization': options.authorization,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Host': 'api.edgecast.com'
+      }
+    };
+
+    this.files.forEach( function ( f ) {
+      if ( ! f.src.length ) {
+        return grunt.fail.warn( 'No source files were found.' );
+      }
+
+      async.eachSeries(f.src,
+        function(filepath, callback){
+
+          var postData = JSON.stringify({
+            "MediaPath": options.edgeCNAME + filepath,
+            "MediaType": options.mediaType
+          });
+
+          var req = http.request( reqOptions, function ( res ) {
+            res.setEncoding( 'utf8' );
+            res.on( 'data', function ( chunk ) {
+              // Don't want to actually log anything here, but the request doesn't seem to work without this function???
+              //grunt.log.writeln( 'BODY: ' + chunk );
+            } );
+            res.on('end', function () {
+              grunt.log.writeln( 'File ' + filepath + ' processed, response code: ' + res.statusCode );
+              //done(res.statusCode);
+              callback();
+            });
+          } );
+
+          req.on( 'error', function ( e ) {
+            grunt.log.writeln( 'problem with request: ' + e.message );
+          } );
+
+          // write data to request body
+          req.write( postData );
+          req.end();
+
+        },
+        function(err){
+          if (err) {
+            grunt.log.error(err);
+          }
+          done(err);
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+      );
 
-      // Handle options.
-      src += options.punctuation;
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+      f.src
+        .forEach( function ( filepath ) {
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+        } );
+    } );
+
+
+  } );
 
 };
